4c6e073d07880561d5db541b9089a21c5e690353
himanushi <himanushi@users.noreply.github.com>
2023-06-18 07:31:39 +0900

    init framework7

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { App as Framework7App, View, Page, Navbar, Toolbar, Link } from 'framework7-react';
+import { App as Framework7App, View, Page, Navbar, Toolbar, Link, Panel, Block, Button, Popup, NavRight } from 'framework7-react';
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -8,22 +8,65 @@ const App = () => {
   return (
     <Framework7App >
 
-    {/* Your main view, should have "main" prop */}
+    {/* Left Panel with "cover" effect */}
+    <Panel left cover>
+      <View>
+        <Page>
+          <Navbar title="Left Panel"></Navbar>
+          <Block>
+            <p>Here comes the left panel text</p>
+          </Block>
+        </Page>
+      </View>
+    </Panel>
+
+    {/* Right Panel with "reveal" effect */}
+    <Panel right reveal>
+      <View>
+        <Page>
+          <Navbar title="Right Panel"></Navbar>
+          <Block>
+            <p>Here comes the right panel text</p>
+          </Block>
+        </Page>
+      </View>
+    </Panel>
+
+    {/*  Main view */}
     <View main>
-      {/*  Initial Page */}
       <Page>
-        {/* Top Navbar */}
         <Navbar title="Awesome App"></Navbar>
-        {/* Toolbar */}
-        <Toolbar bottom>
-          <Link>Link 1</Link>
-          <Link>Link 2</Link>
-        </Toolbar>
-        {/* Page Content */}
-        <p>Page content goes here</p>
-        <Link href="/about/">About App</Link>
+        {/* Page content */}
+        <Block>
+          <p>Here comes main view page text</p>
+        </Block>
+        {/* Buttons to open panels */}
+
+            <Button panelOpen="left">Left Panel</Button>
+
+            <Button panelOpen="right">Right Panel</Button>
+
+        {/* Button to open popup */}
+        <Button popupOpen="#my-popup">Open Popup</Button>
       </Page>
     </View>
+
+    {/* Popup. All modals should be outside of Views */}
+    <Popup id="my-popup">
+      <View>
+        <Page>
+          <Navbar title="Popup">
+            {/* Link to close popup */}
+            <NavRight>
+              <Link popupClose>Close</Link>
+            </NavRight>
+          </Navbar>
+          <Block>
+            <p>Here comes popup text</p>
+          </Block>
+        </Page>
+      </View>
+    </Popup>
   </Framework7App>
   )
 }
