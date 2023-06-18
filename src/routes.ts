89c83b620350caafe032cbdd65618ced1a6d4c22
himanushi <himanushi@users.noreply.github.com>
2023-06-18 14:14:56 +0900

    add routes

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Albums } from "./pages/albums/albums";
+
+export const routes = [
+  {
+    path: "/",
+    component: Albums,
+  },
+  {
+    path: "/albums",
+    component: Albums,
+  },
+];
